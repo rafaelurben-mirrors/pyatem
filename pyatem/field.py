33e3223f9fbeb312c25d4906f993c937bec9d822
Martijn Braam <martijn@brixit.nl>
2026-03-14 13:29:15 +0100

    Fix typo in camera decoder overrides

--- pyatem/field.py
+++ pyatem/field.py
@@ -3354,7 +3354,7 @@ class CameraControlDataPacketField(FieldBase):
             (0, 0): 1,
             (0, 1): 0,
             (0, 2): 1,
-            (0, 3): 1.,
+            (0, 3): 1,
             (0, 4): 1,
             (0, 6): 1,
             (1, 2): 2
